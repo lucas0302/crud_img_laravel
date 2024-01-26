1b2a6b4af62f536bb3b50d17912a38c93f5f53e7
LUCAS  ARAUJO DE OLIVEIRA <lucas.oliveira@lifecon.com.br>
2024-01-26 12:04:54 -0300

    comentarios

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,24 +30,24 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $request->validate([ // validaçao dos capos todos tem que ser obrigatorio
             'name' =>'required',
             'detail'=>'required',
-            'image'=> 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image'=> 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',//o campo e do tipo img e os tipos de arquivos que o campo pode receber e o tamnho max da img.
         ]);
 
         // upload do arquivo
-        $input = $request->all();
+        $input = $request->all(); // pegando tudo do campo
 
-        if($image = $request->file('image')){
-        $destinationPath = 'images/';
-        $profileImage = date("YmdHis") . "." . $image->getClientOriginalExtension();
-        $image->move($destinationPath, $profileImage);
-        $input['image'] = $profileImage;
+        if($image = $request->file('image')){// Ele faz uma verificaçao usando o método $request->file('image'). Se um arquivo 'image' for encontrado na solicitação, ele entra no bloco if.
+        $destinationPath = 'images/';// pasta de destino para onde a imagem será movida após o upload.
+        $profileImage = date("YmdHis") . "." . $image->getClientOriginalExtension();//está sendo gerado um nome de arquivo único para img sendo concatenando a data e hora atual no formato "YmdHis" (ano, mês, dia, hora, minuto e segundo) com a extensão original do arquivo da imagem.
+        $image->move($destinationPath, $profileImage);// Esta linha move o arquivo de imagem para o destino especificado "images/"
+        $input['image'] = $profileImage;//att os dados de entrada (array associativo) para incluir o nome do arquivo da imagem que foi gerado.
         }
 
-        Product::create($input);
-        return redirect()->route('index')->with('success', 'Produto criado com sucesso.');
+        Product::create($input); // iserção dos inputs na table Product do BD
+        return redirect()->route('index')->with('success', 'Produto criado com sucesso.');//redirecionando para a route('index') e retornado uma msg de prod criado
     }
 
     /**
@@ -55,7 +55,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return view('show', compact('product'));
+        return view('show', compact('product'));// visualiza a pag show e com os dados savo na tabela $product
     }
 
     /**
@@ -63,7 +63,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        return view('edit',compact('product'));
+        return view('edit',compact('product'));// visualiza a pag edit e com os dados savo na tabela $product
     }
 
     /**
@@ -71,23 +71,22 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        $request->validate([
-            'name' =>'required',
+        $request->validate([ //faz a verificaçao se os campos tem algo
+            'name' =>'required', // tem que tem argo no campo para continuar
             'detail'=>'required',
         ]);
 
-        $input = $request->all();
+        $input = $request->all(); // pega todos os dados do campo
 
-        if($image = $request->file('image')){
-            $destinationPath = 'images/';
-            $profileImage = date("YmdHis") . "." . $image->getClientOriginalExtension();
-            $image->move($destinationPath, $profileImage);
-            $input['image'] = "$profileImage";
-            }else{
-                unset($input['image']);
+        if($image = $request->file('image')){ //Ele faz uma verificaçao usando o método $request->file('image'). Se um arquivo 'image' for encontrado na solicitação, ele entra no bloco if.
+            $destinationPath = 'images/';// destino que a img vai ser add
+            $profileImage = date("YmdHis") . "." . $image->getClientOriginalExtension();//está sendo gerado um nome de arquivo único para img sendo concatenando a data e hora atual no formato "YmdHis" (ano, mês, dia, hora, minuto e segundo) com a extensão original do arquivo da imagem.
+            $image->move($destinationPath, $profileImage);// Esta linha move o arquivo de imagem para o destino especificado "images/"
+            $input['image'] = $profileImage;//att os dados de entrada (array associativo) para incluir o nome do arquivo da imagem que foi gerado.
+
             }
-            $product->update($input);
-            return redirect()->route('index')->with('success','Produto atualizado com sucesso');
+            $product->update($input); // att a tabela product no bd
+            return redirect()->route('index')->with('success','Produto atualizado com sucesso');//redirecionar e retornar um msg
     }
 
     /**
@@ -95,7 +94,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        $product->delete();
-        return redirect()->route('index')->with('success', 'Produto apagado com sucesso.');
+        $product->delete(); //apaga os dados do bd
+        return redirect()->route('index')->with('success', 'Produto apagado com sucesso.');//redirecionar e retornar um msg
     }
 }
